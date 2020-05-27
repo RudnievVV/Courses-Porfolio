eabd1515ee0875f70a60a529233fb49774c4a287
RudnievVV <RudnievVV@gmail.com>
2020-05-27 23:29:37 +0300

    js console loops

--- index.js
+++ index.js
@@ -69,3 +69,57 @@ function create_table(){
     }
 }
 create_table()
+
+
+let test_global_variable = 1
+function global_variable_changing() {
+    console.log('current global variable is', test_global_variable)
+    test_global_variable = 2
+    console.log('current global variable is', test_global_variable)
+}
+global_variable_changing()
+
+function local_variable_print(){
+    let local_variable = 'local'
+    console.log('local variable is', local_variable, '; and global is', test_global_variable)
+}
+local_variable_print()
+
+var var_variable = 'var_variable'
+function var_changing(var_variable){
+    console.log('current var variable is', var_variable)
+    var var_variable = 123
+    console.log('current var variable is', var_variable)
+}
+var_changing(var_variable)
+
+
+const mockData = ['Bob', 'Alice', 'John', true, 100, 50, { name: "Doe" }]
+for (i in mockData){
+    console.log(mockData[i])
+}
+
+for (i = 0; i < 3; i++){
+    console.log(mockData[i])
+}
+
+var mockDataCounter = 0
+while (mockDataCounter < 3){
+    console.log(mockData[mockDataCounter])
+    mockDataCounter++
+}
+
+var mockDataCounterWithoutObject = 0
+while (mockData[mockDataCounterWithoutObject]){
+    if (mockData[mockDataCounterWithoutObject].constructor != Object){
+        console.log(mockData[mockDataCounterWithoutObject])
+    }
+    mockDataCounterWithoutObject++
+}
+
+mockDataCounterDoWhile = 0
+do {
+    console.log(mockData[mockDataCounterDoWhile])
+    mockDataCounterDoWhile++
+}
+while (mockData[mockDataCounterDoWhile])
