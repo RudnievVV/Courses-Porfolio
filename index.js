7b613c4ffbee6464131c88de1bfdd4b4a9cd02d9
RudnievVV <RudnievVV@gmail.com>
2020-06-05 22:37:09 +0300

    users route for get, post, put, delete methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs  = require('express-handlebars');
+const js2xmlparser = require("js2xmlparser");
 
 const port = 3000
 
@@ -44,6 +45,104 @@ app.get('/skills', function (req, res) {
         my_urls,
     });
 });
+
+app.get('/route/users', function(req, res) {
+    let users = [
+        {
+          name: "Petya",
+          surName: "Petyanov",
+          age: 22,
+          nickName: "pet4uha"
+        },
+        {
+          name: "Vasya",
+          surName: "Vasilkov",
+          age: 44,
+          nickName: "vasile4ek"
+        },
+        {
+          name: "Ivan",
+          surName: "Ivanov",
+          age: 66,
+          nickName: "ivanovi4"
+        },
+        {
+          name: "Abdula",
+          surName: "Abrahamanov",
+          age: 88,
+          nickName: "ibnSallah"
+        }
+    ]
+    if (req.headers["content-type"]){
+        if (req.headers["content-type"] == 'application/json') {
+            res.send(users);
+        }
+        else if (req.headers["content-type"] == 'application/xml') {
+            res.send(js2xmlparser.parse('users', users));
+        }
+        else {
+            res.send('bad request')
+        };
+    }
+    else {
+        // res.send('bad request')
+        res.send(users);
+    };
+});
+
+app.route('/route/users')
+    .get(function(req, res) {
+        let users = [
+            {
+              name: "Petya",
+              surName: "Petyanov",
+              age: 22,
+              nickName: "pet4uha"
+            },
+            {
+              name: "Vasya",
+              surName: "Vasilkov",
+              age: 44,
+              nickName: "vasile4ek"
+            },
+            {
+              name: "Ivan",
+              surName: "Ivanov",
+              age: 66,
+              nickName: "ivanovi4"
+            },
+            {
+              name: "Abdula",
+              surName: "Abrahamanov",
+              age: 88,
+              nickName: "ibnSallah"
+            }
+        ]
+        if (req.headers["content-type"]){
+            if (req.headers["content-type"] == 'application/json') {
+                res.send(users);
+            }
+            else if (req.headers["content-type"] == 'application/xml') {
+                res.send(js2xmlparser.parse('users', users));
+            }
+            else {
+                res.send('bad request')
+            };
+        }
+        else {
+            // res.send('bad request')
+            res.send(users);
+        };
+    })
+    .post(function (req, res) {
+    res.status(405).send('POST Method Not Allowed')
+    })
+    .put(function (req, res) {
+    res.status(405).send('PUT Method Not Allowed')
+    })
+    .delete(function (req, res) {
+    res.status(405).send('DELETE Method Not Allowed')
+    })
 
 app.listen(port, function () {
   console.log(`My portfolio listening on port ${port}!`);
